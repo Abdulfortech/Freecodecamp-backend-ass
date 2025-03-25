466ec4d6583bb9acf604bd132e6fb8b6efeebf41
abdulfortech <ibnmudi@gmail.com>
2025-03-25 23:55:34 +0100

    changes

--- myApp.js
+++ myApp.js
@@ -12,16 +12,32 @@ app.get('/', function(req, res) {
     res.sendFile(absolutePath);
 });
 
+// Middleware to add current time to request object
+const addTimeMiddleware = (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+};
+
+// Chained route with middleware and handler
+app.get('/now', addTimeMiddleware, (req, res) => {
+  res.json({ time: req.time });
+});
+
+// Start the server (optional, for testing purposes)
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 // /now route handler with chained middleware
-app.get('/now', function (req, res, next) {
-    req.time = new Date().toUTCString().replace('GMT', 'UTC');;
-    next();
-  },
-  function (req, res) {
-    res.send({
-      time: req.time
-    });
-  });
+// app.get('/now', function (req, res, next) {
+//     req.time = new Date().toUTCString().replace('GMT', 'UTC');;
+//     next();
+//   },
+//   function (req, res) {
+//     res.send({
+//       time: req.time
+//     });
+//   });
 
 
 
